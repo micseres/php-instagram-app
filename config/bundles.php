2c20eeda5c2774e0cae4e37326cfe3e949baf8ff
zogxray <zogxray@gmail.com>
2019-11-07 15:38:52 -0100

    master send error emails

--- config/bundles.php
+++ config/bundles.php
@@ -2,8 +2,12 @@
 
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use GepurIt\RabbitMqBundle\RabbitMqBundle;
+use Symfony\Bundle\MonologBundle\MonologBundle;
+use Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle;
 
 return [
+    MonologBundle::class => ['all' => true],
+    SwiftmailerBundle::class => ['all' => true],
     FrameworkBundle::class => ['all' => true],
     RabbitMqBundle::class => ['all' => true],
 ];
